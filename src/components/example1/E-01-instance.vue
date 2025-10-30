c39d9ba93d50bc6e707c0c5acd6f053829169863
Milku <ptrol123@naver.com>
2025-10-30 19:38:40 +0900

    Initial commit

---
+++ src/components/example1/E-01-instance.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Hello, {{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "E01Instance",
+  data() {
+    const name = 'Park Minho'
+
+    return{
+      message: name,
+    };
+  }
+};
+</script>
+
+<style scoped>
+div {
+  color: blue;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/example1/E-02-reactive.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>{{ fullName }}</div>
+</template>
+
+<script>
+export default {
+  name: "E02Reactive",
+  data() {
+    return {
+      firstName: "Minho",
+      lastName: "Park"
+    };
+  },
+  mounted() {
+    console.log('mounted');
+
+    let a = 10;
+
+    setTimeout(() => {
+      console.log('setTimeout');
+      a = 20;
+      console.log(a);
+    }, 500);
+
+    setTimeout(() => {
+      this.firstName = "123";
+    }, 1000);
+
+    setTimeout(() => {
+      this.firstName = "PMH";
+    }, 2000);
+  },
+  computed: {
+    fullName() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+};
+</script>
